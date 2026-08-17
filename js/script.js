089d3b17f18ca239c9e66d415181cbea444e6d49
luisanaacopa <luisanacopa16@gmail.com>
2026-08-16 21:51:02 -0300

    feat(js): quitar clase titulo-feo del h1
    Closes #5

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 //1- Agregar a la tarjeta, el atributo "class" con el valor "card"
 const tarjeta = document.getElementById('tarjeta');
-
 tarjeta.setAttribute('class', 'card');
 
 //2- Agregar a la imagen, el atributo "src" con el valor "https://www.youtube.com/img/desktop/yt_1200.png"
 const logo = document.getElementById('logo');
-
 logo.setAttribute('src', 'https://www.youtube.com/img/desktop/yt_1200.png');
+
 //3- Quitarle al titulo la clase que le está dando un formato feo
+const titulo = document.querySelector('.titulo-feo');
+titulo.classList.remove('titulo-feo');
 
 //4- Chequear si el link a youtube posee o no el atributo href
 
